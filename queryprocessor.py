08dea67bfa1f1b7e4dc570c2f9cbffccc3bd11ed
Mike Lancaster <mike.lancaster@db.com>
2020-06-24 15:22:19 +0100

    update

--- queryprocessor.py
+++ queryprocessor.py
@@ -3,4 +3,6 @@ def process_query(query):
         return "William Shakespeare"
     if ("what is your name" in query.lower()):
         return "Mike Lancaster"
+    if ("which of the following numbers is the largest"
+        return "20880"
     return ""
