77977356e76ba67e2f458380295a36550a4d3edd
Mike Lancaster <mike.lancaster@db.com>
2020-06-24 15:01:50 +0100

    test

--- queryprocessor.py
+++ queryprocessor.py
@@ -1,4 +1,4 @@
 def process_query(query):
-	if ("romeo and juliet" in query.lower()):
-		return "William Shakespeare"
+	if ("what is your name" in query.lower()):
+		return "Mike Lancaster"
 	return ""
