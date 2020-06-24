8beae1edefa902e7d71c9d34deb6dfff867eb0a7
Mike Lancaster <mike.lancaster@db.com>
2020-06-24 16:00:03 +0100

    update

--- queryprocessor.py
+++ queryprocessor.py
@@ -5,10 +5,11 @@ def process_query(query):
         return "Mike Lancaster"
     if ("which of the following numbers is the largest" in query.lower()):
         #print max(query.split(":")[1].split(","))
-        numbers = list(map(int,query.split(":")[1].split(",")))
-        return max(numbers)
+        print map(int,query.split(":")[1].split(","))
+        return max(map(int,query.split(":")[1].split(",")))
     if ("plus" in query.lower()):
         return sum([int(s) for s in query.split() if s.isdigit()])
     if ("which year was Theresa May first elected" in query.lower()):
         return "2016"
+    
     return ""
