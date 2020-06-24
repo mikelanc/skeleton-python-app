44bd32b6ebaf8f5ee4c98681c6390b5e48961557
Mike Lancaster <mike.lancaster@db.com>
2020-06-24 15:37:37 +0100

    update

--- queryprocessor.py
+++ queryprocessor.py
@@ -4,7 +4,7 @@ def process_query(query):
     if ("what is your name" in query.lower()):
         return "Mike Lancaster"
     if ("which of the following numbers is the largest" in query.lower()):
-        return query.split(":").max()
+        return max(query.split(":"))
     if ("plus" in query.lower()):
-        return [int(s) for s in query.split() if s.isdigit()].sum()
+        return sum([int(s) for s in query.split() if s.isdigit()])
     return ""
