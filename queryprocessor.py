e4da98ee9bb4e4fd8bb6c72b64a244da13018cf5
Mike Lancaster <mike.lancaster@db.com>
2020-06-24 16:21:02 +0100

    update

--- queryprocessor.py
+++ queryprocessor.py
@@ -1,3 +1,17 @@
+def is_prime(n):
+  if n == 2 or n == 3: return True
+  if n < 2 or n%2 == 0: return False
+  if n < 9: return True
+  if n%3 == 0: return False
+  r = int(n**0.5)
+  f = 5
+  while f <= r:
+    print('\t',f)
+    if n % f == 0: return False
+    if n % (f+2) == 0: return False
+    f += 6
+  return True    
+
 def process_query(query):
     if ("romeo and juliet" in query.lower()):
         return "William Shakespeare"
@@ -6,7 +20,10 @@ def process_query(query):
     if ("which of the following numbers is the largest" in query.lower()):
         #print max(query.split(":")[1].split(","))
         #print map(int,query.split(":")[1].split(","))
-        return max(map(int,query.split(":")[1].split(",")))
+        #return max(map(int,query.split(":")[1].split(",")))
+        test_list = query.split(":")[1].split(",")
+        test_list = list(map(int, test_list)) 
+        return max(test_list)
     if ("plus" in query.lower()):
         return sum([int(s) for s in query.split() if s.isdigit()])
     if ("which year was Theresa May first elected" in query.lower()):
@@ -15,4 +32,13 @@ def process_query(query):
         return "Sean Connery"
     if ("eiffel" in query.lower()):
         return "Paris"
+    if ("are primes" in query.lower()):
+        test_list = query.split(":")[1].split(",")
+        test_list = list(map(int, test_list)) 
+        newlist = []
+        for i in test_list:
+            if is_prime(i):
+                newlist.append(i)
+        return newlist
+        #:%20457,%20586,%20353,%20467
     return ""
